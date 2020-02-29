612e666a88fc01d9f0f5eb1467c3f4e226b52c0d
Aji <fajriajjha@gmail.com>
2020-03-01 00:30:57 +0700

    perbaiki private route

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -1,12 +1,13 @@
 import express from 'express'
+import wrapperRequest from '../helpers/ExpressHelpers'
 /* Declare Controllers */
 import RoleController from '../controllers/RoleController'
 
 const router = express.Router()
 
 /* Role */
-router.post('/role', RoleController.create)
-router.put('/role/:id', RoleController.update)
-router.delete('/role/:id', RoleController.destroy)
+router.post('/role', wrapperRequest(RoleController.create))
+router.put('/role/:id', wrapperRequest(RoleController.update))
+router.delete('/role/:id', wrapperRequest(RoleController.destroy))
 
 export default router
