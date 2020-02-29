15e39fafd2864517b87e4be5494f6dd4fadaa281
Aji <fajriajjha@gmail.com>
2020-03-01 00:27:45 +0700

    add babel config

--- src/controllers/base/BaseController.ts
+++ src/controllers/base/BaseController.ts
@@ -1,6 +1,4 @@
 /* eslint-disable no-unused-vars */
-import { Request, Response } from 'express'
-
 function createBaseController(
   modelMaster: any,
   modelValidation: any,
@@ -8,84 +6,62 @@ function createBaseController(
 ) {
   const { configGetAll, confitGetOne } = options || {}
 
-  async function getAll(req: Request, res: Response) {
-    try {
-      const data = await modelMaster.findAll()
+  async function getAll({ req, ResponseError }: any) {
+    const data = await modelMaster.findAll()
 
-      const totalRow = await modelMaster.count()
+    const totalRow = await modelMaster.count()
 
-      return res.status(200).json({ data, totalRow })
-    } catch (e) {
-      return res.status(400).json({ message: e.message })
-    }
+    return { data, totalRow }
   }
 
-  async function getOne(req: Request, res: Response) {
+  async function getOne({ req, ResponseError }: any) {
     const { params } = req
-    try {
-      const data = await modelMaster.findByPk(params.id)
+    const data = await modelMaster.findByPk(params.id)
 
-      if (!data) {
-        return res
-          .status(400)
-          .json({ message: 'Data tidak ditemukan atau sudah terhapus!' })
-      }
-
-      return res.status(200).json({ data })
-    } catch (e) {
-      return res.status(400).json({ message: e.message })
+    if (!data) {
+      throw new ResponseError({
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+      })
     }
+
+    return { data }
   }
 
-  async function create(req: Request, res: Response) {
+  async function create({ req, ResponseError }: any) {
     const { body } = req
-    try {
-      const rawFormData = { ...body }
-      const data = await modelMaster.create(rawFormData)
+    const rawFormData = { ...body }
+    const data = await modelMaster.create(rawFormData)
 
-      return res
-        .status(200)
-        .json({ data, message: 'Data berhasil ditambahkan!' })
-    } catch (e) {
-      return res.status(400).json({ message: e.message })
-    }
+    return { data, message: 'Data berhasil ditambahkan!' }
   }
 
-  async function update(req: Request, res: Response) {
+  async function update({ req, ResponseError }: any) {
     const { body, params } = req
-    try {
-      const rawFormData = { ...body }
-      const data = await modelMaster.findByPk(params.id)
+    const rawFormData = { ...body }
+    const data = await modelMaster.findByPk(params.id)
 
-      if (!data) {
-        return res
-          .status(400)
-          .json({ message: 'Data tidak ditemukan atau sudah terhapus!' })
-      }
-
-      await data.update(rawFormData)
-      return res.status(200).json({ data, message: 'Data sudah diperbarui!' })
-    } catch (e) {
-      return res.status(400).json({ message: e.message })
+    if (!data) {
+      throw new ResponseError({
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+      })
     }
+
+    await data.update(rawFormData)
+    return { data, message: 'Data sudah diperbarui!' }
   }
 
-  async function destroy(req: Request, res: Response) {
+  async function destroy({ req, ResponseError }: any) {
     const { params } = req
-    try {
-      const data = await modelMaster.findByPk(params.id)
+    const data = await modelMaster.findByPk(params.id)
 
-      if (!data) {
-        return res
-          .status(400)
-          .json({ message: 'Data tidak ditemukan atau sudah terhapus!' })
-      }
-
-      await data.destroy()
-      return res.status(200).json({ message: 'Data berhasil dihapus!' })
-    } catch (e) {
-      return res.status(400).json({ message: e.message })
+    if (!data) {
+      throw new ResponseError({
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+      })
     }
+
+    await data.destroy()
+    return { message: 'Data berhasil dihapus!' }
   }
 
   return { getAll, getOne, create, update, destroy }
--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,11 +1,12 @@
 import express from 'express'
+import wrapperRequest from '../helpers/ExpressHelpers'
 /* Declare Controllers */
 import RoleController from '../controllers/RoleController'
 
 const router = express.Router()
 
 /* Role */
-router.get('/role', RoleController.getAll)
-router.get('/role/:id', RoleController.getOne)
+router.get('/role', wrapperRequest(RoleController.getAll))
+router.get('/role/:id', wrapperRequest(RoleController.getOne))
 
 export default router
