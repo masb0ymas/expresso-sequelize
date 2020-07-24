58ec2f099c4dd9bbccf17eae9c2ebf6da8acf1df
Coeg <fajriajjha@gmail.com>
2020-07-24 21:42:37 +0700

    fix: routes role controller

--- src/controllers/Role/controller.ts
+++ src/controllers/Role/controller.ts
@@ -2,73 +2,86 @@
 import models from 'models'
 import { Request, Response } from 'express'
 import useValidation from 'helpers/useValidation'
+import routes from 'routes/public'
+import asyncHandler from 'helpers/asyncHandler'
 import schema from './schema'
 
 const { Role } = models
 
-async function getAll(req: Request, res: Response) {
-  const data = await Role.findAll()
-  const total = await Role.count()
+routes.get(
+  '/role',
+  asyncHandler(async function getAll(req: Request, res: Response) {
+    const data = await Role.findAll()
+    const total = await Role.count()
 
-  return res.status(200).json({ data, total })
-}
-
-async function getOne(req: Request, res: Response) {
-  const { id } = req.params
-  const data = await Role.findByPk(id)
-
-  if (!data) {
-    return res.status(404).json({
-      message: 'Data tidak ditemukan atau sudah terhapus!',
-    })
-  }
-
-  return res.status(200).json({ data })
-}
-
-async function createRole(req: Request, res: Response) {
-  const value = useValidation(schema.create, req.getBody())
-  const data = await Role.create(value)
-
-  return res.status(201).json({ data })
-}
-
-async function updateRole(req: Request, res: Response) {
-  const { id } = req.params
-
-  const data = await Role.findByPk(id)
-
-  if (!data) {
-    return res.status(404).json({
-      message: 'Data tidak ditemukan atau sudah terhapus!',
-    })
-  }
-
-  const value = useValidation(schema.create, {
-    ...data.toJSON(),
-    ...req.getBody(),
+    return res.status(200).json({ data, total })
   })
+)
 
-  await data.update(value || {})
-  return res.status(200).json({ data })
-}
+routes.get(
+  '/role/:id',
+  asyncHandler(async function getOne(req: Request, res: Response) {
+    const { id } = req.params
+    const data = await Role.findByPk(id)
 
-async function deleteRole(req: Request, res: Response) {
-  const { id } = req.params
-  const data = await Role.findByPk(id)
+    if (!data) {
+      return res.status(404).json({
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+      })
+    }
 
-  if (!data) {
-    return res.status(404).json({
-      message: 'Data tidak ditemukan atau sudah terhapus!',
-    })
-  }
-
-  await data.destroy()
-  return res.status(200).json({
-    message: 'success',
+    return res.status(200).json({ data })
   })
-}
+)
 
-const RoleController = { getAll, getOne, createRole, updateRole, deleteRole }
+routes.post(
+  '/role',
+  asyncHandler(async function createRole(req: Request, res: Response) {
+    const value = useValidation(schema.create, req.getBody())
+    const data = await Role.create(value)
 
-module.exports = RoleController
+    return res.status(201).json({ data })
+  })
+)
+
+routes.put(
+  '/role/:id',
+  asyncHandler(async function updateRole(req: Request, res: Response) {
+    const { id } = req.params
+
+    const data = await Role.findByPk(id)
+
+    if (!data) {
+      return res.status(404).json({
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+      })
+    }
+
+    const value = useValidation(schema.create, {
+      ...data.toJSON(),
+      ...req.getBody(),
+    })
+
+    await data.update(value || {})
+    return res.status(200).json({ data })
+  })
+)
+
+routes.delete(
+  '/role/:id',
+  asyncHandler(async function deleteRole(req: Request, res: Response) {
+    const { id } = req.params
+    const data = await Role.findByPk(id)
+
+    if (!data) {
+      return res.status(404).json({
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+      })
+    }
+
+    await data.destroy()
+    return res.status(200).json({
+      message: 'Data berhasil dihapus!',
+    })
+  })
+)
--- src/routes/public.ts
+++ src/routes/public.ts
@@ -2,12 +2,6 @@ import express from 'express'
 
 const router = express.Router()
 
-const RoleController = require('controllers/Role/controller')
-
-router.get('/role', RoleController.getAll)
-router.get('/role/:id', RoleController.getOne)
-router.post('/role', RoleController.createRole)
-router.put('/role/:id', RoleController.updateRole)
-router.delete('/role/:id', RoleController.deleteRole)
-
 export default router
+
+require('controllers/Role/controller')
