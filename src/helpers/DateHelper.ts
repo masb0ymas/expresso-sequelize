ec31bc161d123e9e5c0c99b027dbbb39ec2dff50
Coeg <fajriajjha@gmail.com>
2020-07-25 11:44:33 +0700

    fix: typing date helper

--- src/helpers/DateHelper.ts
+++ src/helpers/DateHelper.ts
@@ -3,31 +3,31 @@ import { id } from 'date-fns/locale'
 
 const timezoneIndonesia = { locale: id }
 
-const formatDate = (date) => {
+const formatDate = (date: Date | number | string) => {
   return format(new Date(date), 'dd-MM-yyyy', timezoneIndonesia)
 }
 
-const formatDateTime = (date) => {
+const formatDateTime = (date: Date | number | string) => {
   return format(new Date(date), 'dd-MM-yyyy HH:mm:ss', timezoneIndonesia)
 }
 
-const formatDateSystem = (date) => {
+const formatDateSystem = (date: Date | number | string) => {
   return format(new Date(date), 'yyyy-MM-dd', timezoneIndonesia)
 }
 
-const formatDateTimeSystem = (date) => {
+const formatDateTimeSystem = (date: Date | number | string) => {
   return format(new Date(date), 'yyyy-MM-dd HH:mm:ss', timezoneIndonesia)
 }
 
-const formatMonth = (date) => {
+const formatMonth = (date: Date | number | string) => {
   return format(new Date(date), 'MMMM', timezoneIndonesia)
 }
 
-const formatYear = (date) => {
+const formatYear = (date: Date | number | string) => {
   return format(new Date(date), 'yyyy', timezoneIndonesia)
 }
 
-const formatTime = (date) => {
+const formatTime = (date: Date | number | string) => {
   return format(new Date(date), 'HH:mm:ss', timezoneIndonesia)
 }
 
