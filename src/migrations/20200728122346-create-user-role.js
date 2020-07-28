0a68e6d5aed8db7bbc5e765d8f7c4d4b845fd198
masb0ymas <fajriajjha@gmail.com>
2020-07-28 23:02:16 +0700

    add: model user role

---
+++ src/migrations/20200728122346-create-user-role.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UserRoles', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      UserId: {
+        type: Sequelize.UUID,
+      },
+      RoleId: {
+        type: Sequelize.UUID,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UserRoles')
+  },
+}
---
+++ src/models/userrole.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import { Model, Optional } from 'sequelize'
+import SequelizeAttributes from '../utils/SequelizeAttributes'
+
+import db from './_instance'
+
+interface UserRoleAttributes {
+  id: string
+  UserId: string
+  RoleId: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+interface UserRoleCreationAttributes
+  extends Optional<UserRoleAttributes, 'id'> {}
+
+interface UserRoleInstance
+  extends Model<UserRoleAttributes, UserRoleCreationAttributes>,
+    UserRoleAttributes {}
+
+const UserRole = db.sequelize.define<UserRoleInstance>('UserRoles', {
+  ...SequelizeAttributes.UserRoles,
+})
+
+export default UserRole
