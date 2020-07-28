256ee77d4f81ee64dd88424b18c3f1a6898eea8e
masb0ymas <fajriajjha@gmail.com>
2020-07-28 23:01:48 +0700

    add: auth in public route

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import passport from 'passport'
 
 const router = express.Router()
 
+require('config/passport')(passport)
+
+/* always check auth */
+export const AuthMiddleware = passport.authenticate('jwt', { session: false })
+
 export default router
 
+require('controllers/Auth/controller')
 require('controllers/Role/controller')
