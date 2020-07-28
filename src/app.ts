e6a56c2fc3da00ccbc2328311d16feab48ccc0c2
masb0ymas <fajriajjha@gmail.com>
2020-07-28 23:01:08 +0700

    fix: cors app.ts

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.set('views', path.join(`${__dirname}/../`, 'views'))
 app.set('view engine', 'pug')
 
 app.use(helmet())
-app.all('*', cors())
+app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
