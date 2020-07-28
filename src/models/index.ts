760f1601e682b0afc87ea1b2087e50bb16ca373c
masb0ymas <fajriajjha@gmail.com>
2020-07-28 23:02:47 +0700

    add: model association

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,15 @@
-import Role from './Role'
-import User from './User'
+import Role from './role'
+import User from './user'
+import UserRole from './userrole'
 
 export default {
   Role,
   User,
+  UserRole,
 }
+
+/*
+  Models Association
+*/
+
+User.belongsToMany(Role, { through: UserRole })
