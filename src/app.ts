c315562af4734a155649426099d8a5f03030b28f
masb0ymas <fajriajjha@gmail.com>
2020-07-30 22:38:24 +0700

    fix: global response error

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,12 @@ import path from 'path'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-import * as yup from 'yup'
-import { get, isObject } from 'lodash'
-import Sequelize from 'sequelize'
 import indexRouter from 'routes'
 import withState from 'helpers/withState'
 import models from 'models/_instance'
-import ResponseError from 'modules/ResponseError'
+import ExpressErrorYup from 'middlewares/ExpressErrorYup'
+import ExpressErrorResponse from 'middlewares/ExpressErrorResponse'
+import ExpressErrorSequelize from 'middlewares/ExpressErrorSequelize'
 
 const GenerateDoc = require('utils/GenerateDocs')
 const logger = require('morgan')
@@ -50,57 +49,9 @@ models.sequelize
 // Initial Route
 app.use(indexRouter)
 
-function generateErrorResponseError(e: Error) {
-  return isObject(e.message) ? e.message : { message: e.message }
-}
-
-app.use('/v1', async function (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  await req.rollbackTransactions()
-
-  if (err instanceof ResponseError.BaseResponse) {
-    return res.status(err.statusCode).json(generateErrorResponseError(err))
-  }
-
-  if (err instanceof yup.ValidationError) {
-    console.log('ERROR YUP VALIDATION!!!')
-    const error = {
-      message: err.errors.join('<br/>') || 'Yup Validation Error !',
-      errors:
-        err.inner.length > 0
-          ? err.inner.reduce((acc: any, curVal: any) => {
-              acc[`${curVal.path}`] = curVal.message || curVal.type
-              return acc
-            }, {})
-          : { [`${err.path}`]: err.message || err.type },
-    }
-    return res.status(422).json(error)
-  }
-
-  if (err instanceof Sequelize.ValidationError) {
-    console.log('ERROR SEQUELIZE VALIDATION!!!')
-    const errors: any[] = get(err, 'errors', [])
-    const errorMessage = get(errors, '0.message', null)
-
-    const dataError = {
-      message: errorMessage ? `Validation error: ${errorMessage}` : err.message,
-      errors: errors.reduce<any>((acc, curVal) => {
-        acc[curVal.path] = curVal.message
-        return acc
-      }, {}),
-    }
-
-    console.log(dataError.message, dataError.errors)
-
-    return res.status(400).json(dataError)
-  }
-
-  next(err)
-})
+app.use(ExpressErrorYup)
+app.use(ExpressErrorSequelize)
+app.use(ExpressErrorResponse)
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
