422b877bfcade6215a116a98dfd167182f427a54
masb0ymas <fajriajjha@gmail.com>
2020-07-30 22:37:14 +0700

    fix: object prototype forbidden

--- src/modules/Errors/Forbidden.ts
+++ src/modules/Errors/Forbidden.ts
@@ -3,6 +3,7 @@ import BaseResponse from './BaseResponse'
 class Forbidden extends BaseResponse {
   constructor(message: string) {
     super(message, 403)
+    Object.setPrototypeOf(this, Forbidden.prototype)
   }
 }
 
