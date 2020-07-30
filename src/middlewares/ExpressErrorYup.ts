71cdc276abee28bf44e1aac92621b43f6de4856b
masb0ymas <fajriajjha@gmail.com>
2020-07-30 22:36:00 +0700

    add: express error yup

---
+++ src/middlewares/ExpressErrorYup.ts
@@ -0,0 +1,31 @@
+// eslint-disable-next-line no-unused-vars
+import { NextFunction, Request, Response } from 'express'
+import { ValidationError } from 'yup'
+
+async function ExpressErrorYup(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  await req.rollbackTransactions()
+
+  if (err instanceof ValidationError) {
+    console.log('ERROR YUP VALIDATION!!!')
+    const error = {
+      message: err.errors.join('<br/>') || 'Yup Validation Error !',
+      errors:
+        err.inner.length > 0
+          ? err.inner.reduce((acc: any, curVal: any) => {
+              acc[`${curVal.path}`] = curVal.message || curVal.type
+              return acc
+            }, {})
+          : { [`${err.path}`]: err.message || err.type },
+    }
+    return res.status(422).json(error)
+  }
+
+  next(err)
+}
+
+export default ExpressErrorYup
