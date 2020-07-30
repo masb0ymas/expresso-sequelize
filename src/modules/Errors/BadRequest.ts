538acac8d70b46ed9653372dcec22d29d630ec65
masb0ymas <fajriajjha@gmail.com>
2020-07-30 22:36:53 +0700

    fix: object prototype bad request

--- src/modules/Errors/BadRequest.ts
+++ src/modules/Errors/BadRequest.ts
@@ -3,6 +3,7 @@ import BaseResponse from './BaseResponse'
 class BadRequest extends BaseResponse {
   constructor(message: string) {
     super(message, 400)
+    Object.setPrototypeOf(this, BadRequest.prototype)
   }
 }
 
