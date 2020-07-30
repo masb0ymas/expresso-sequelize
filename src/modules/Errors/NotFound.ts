7dde81fcbeba7692b9627c336e1cd298bac74975
masb0ymas <fajriajjha@gmail.com>
2020-07-30 22:37:22 +0700

    fix: object prototype not found

--- src/modules/Errors/NotFound.ts
+++ src/modules/Errors/NotFound.ts
@@ -3,6 +3,7 @@ import BaseResponse from './BaseResponse'
 class NotFound extends BaseResponse {
   constructor(message: string) {
     super(message, 404)
+    Object.setPrototypeOf(this, NotFound.prototype)
   }
 }
 
