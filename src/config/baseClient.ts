89726f94657da6e3edef267e01775ba8908465a3
masb0ymas <fajriajjha@gmail.com>
2020-08-15 12:28:43 +0700

    fix: config base client

--- src/config/baseClient.ts
+++ src/config/baseClient.ts
@@ -1,7 +1,7 @@
 const BASE_URL = {
   development: 'http://localhost:3000',
-  staging: 'http://minangitcamp.com',
-  production: 'http://minangitcamp.com',
+  staging: 'http://staging.example.com',
+  production: 'http://example.com',
 }
 
 const ENV = process.env.NODE_ENV || 'development'
