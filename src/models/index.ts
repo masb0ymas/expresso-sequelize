693b6ff88f65a5767cfe16444f08f75abe28a8be
masb0ymas <fajriajjha@gmail.com>
2020-08-19 23:13:40 +0700

    add: interface pagination

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,23 @@ import Role from './role'
 import User from './user'
 import UserRole from './userrole'
 
+export interface FilterAttributes {
+  id: string
+  value: string
+}
+
+export interface SortAttributes {
+  id: string
+  desc: string
+}
+
+export interface FilterQueryAttributes {
+  page: string | number
+  pageSize: string | number
+  filtered: string
+  sorted: string
+}
+
 export default {
   Role,
   User,
