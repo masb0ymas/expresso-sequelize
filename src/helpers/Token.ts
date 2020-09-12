cb0083938f77d28c843747d3ff2336ce068398ae
masb0ymas <fajriajjha@gmail.com>
2020-09-12 16:19:12 +0700

    feat: add helper token

---
+++ src/helpers/Token.ts
@@ -0,0 +1,49 @@
+import jwt, {
+  TokenExpiredError,
+  JsonWebTokenError,
+  NotBeforeError,
+} from 'jsonwebtoken'
+
+require('dotenv').config()
+
+const { JWT_SECRET }: any = process.env
+
+// Get Token from headers
+function getToken(headers: any) {
+  if (headers && headers.authorization) {
+    const parted = headers.authorization.split(' ')
+    if (parted.length === 2) {
+      return parted[1]
+    }
+    return null
+  }
+  return null
+}
+
+// Verify Token
+function verifyToken(header: any) {
+  const token = getToken(header)
+
+  try {
+    if (!token) {
+      return { data: null, message: 'Token not found!' }
+    }
+
+    const data = jwt.verify(token, JWT_SECRET)
+    return { data, message: 'Token is verify' }
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+
+    if (err instanceof JsonWebTokenError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+
+    if (err instanceof NotBeforeError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+  }
+}
+
+export { getToken, verifyToken }
