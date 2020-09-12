62ba57d43191e242c14addcca35c8fd5b375fafb
masb0ymas <fajriajjha@gmail.com>
2020-09-12 16:19:31 +0700

    fix: route private

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -1,13 +1,6 @@
 import express from 'express'
-import passport from 'passport'
 
 const router = express.Router()
-
-require('config/passport')(passport)
-
-/* always check auth */
-const AuthMiddleware = passport.authenticate('jwt', { session: false })
-router.use(AuthMiddleware)
 
 export default router
 
