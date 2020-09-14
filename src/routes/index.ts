0f7155a0a981e1a166fd98175041a9206d21ab4f
masb0ymas <fajriajjha@gmail.com>
2020-09-14 22:01:15 +0700

    fix: remove private route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import express, { Request, Response, NextFunction } from 'express'
 import publicRoute from './public'
-import privateRoute from './private'
 
 const router = express.Router()
 
@@ -24,7 +23,6 @@ router.get('/v1', function (req: Request, res: Response, next: NextFunction) {
 
 /* Declare Route */
 router.use('/v1', publicRoute)
-router.use('/v1', privateRoute)
 
 /* Not Found Page. */
 router.get('*', function (req: Request, res: Response, next: NextFunction) {
--- src/routes/private.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-export default router
-
-require('controllers/User/controller')
