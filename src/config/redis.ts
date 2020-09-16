f683721f0545276cf052646c0dbafe39fa47595e
masb0ymas <fajriajjha@gmail.com>
2020-09-16 17:17:19 +0700

    fix: config redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -10,12 +10,18 @@ client.on('error', function (err) {
   console.log(`Something went wrong ${err}`)
 })
 
-export function redisCache(key: string, data: any[]): void {
-  client.setex(key, 86400, JSON.stringify(data))
+class Redis {
+  public static set(key: string, data: any[]) {
+    client.setex(key, 86400, JSON.stringify(data))
+  }
+
+  public static get(key: string) {
+    client.get(key)
+  }
+
+  public static del(key: string) {
+    client.del(key)
+  }
 }
 
-export function redisDeleteCache(key: string) {
-  client.del(key)
-}
-
-export default client
+export default Redis
