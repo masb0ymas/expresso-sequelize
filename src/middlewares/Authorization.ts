724c3e01f013955801ad8cdbf6a2f054d540f284
masb0ymas <fajriajjha@gmail.com>
2020-10-20 20:26:05 +0700

    fix: get current token auth middleware

--- src/middlewares/Authorization.ts
+++ src/middlewares/Authorization.ts
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import { NextFunction, Request, Response } from 'express'
-import { verifyToken } from 'helpers/Token'
+import { currentToken, verifyToken } from 'helpers/Token'
 import { isEmpty } from 'lodash'
 
 async function Authorization(req: Request, res: Response, next: NextFunction) {
-  const token = verifyToken(req.getHeaders())
+  const getToken = currentToken(req)
+  const token = verifyToken(getToken)
 
   if (isEmpty(token?.data)) {
     return res.status(401).json({
