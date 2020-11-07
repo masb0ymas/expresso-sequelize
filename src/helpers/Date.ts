a35072c1d8a1b361bc3bdb28ca8dca2ae5a17c10
masb0ymas <fajriajjha@gmail.com>
2020-11-07 14:02:56 +0700

    fix: helper date

--- src/helpers/Date.ts
+++ src/helpers/Date.ts
@@ -1,34 +1,34 @@
 import { format } from 'date-fns'
 import { id } from 'date-fns/locale'
 
-const timezoneIndonesia = { locale: id }
+const TZ_ID = { locale: id } // Timezone Indonesia
 
 const formatDate = (date: Date | number | string) => {
-  return format(new Date(date), 'dd-MM-yyyy', timezoneIndonesia)
+  return format(new Date(date), 'dd-MM-yyyy', TZ_ID)
 }
 
 const formatDateTime = (date: Date | number | string) => {
-  return format(new Date(date), 'dd-MM-yyyy HH:mm:ss', timezoneIndonesia)
+  return format(new Date(date), 'dd-MM-yyyy HH:mm:ss', TZ_ID)
 }
 
 const formatDateSystem = (date: Date | number | string) => {
-  return format(new Date(date), 'yyyy-MM-dd', timezoneIndonesia)
+  return format(new Date(date), 'yyyy-MM-dd', TZ_ID)
 }
 
 const formatDateTimeSystem = (date: Date | number | string) => {
-  return format(new Date(date), 'yyyy-MM-dd HH:mm:ss', timezoneIndonesia)
+  return format(new Date(date), 'yyyy-MM-dd HH:mm:ss', TZ_ID)
 }
 
 const formatMonth = (date: Date | number | string) => {
-  return format(new Date(date), 'MMMM', timezoneIndonesia)
+  return format(new Date(date), 'MMMM', TZ_ID)
 }
 
 const formatYear = (date: Date | number | string) => {
-  return format(new Date(date), 'yyyy', timezoneIndonesia)
+  return format(new Date(date), 'yyyy', TZ_ID)
 }
 
 const formatTime = (date: Date | number | string) => {
-  return format(new Date(date), 'HH:mm:ss', timezoneIndonesia)
+  return format(new Date(date), 'HH:mm:ss', TZ_ID)
 }
 
 export {
