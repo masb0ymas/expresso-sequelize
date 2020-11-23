33eba8fc2a1eccf9d0dc96615af7835ee2e3470c
masb0ymas <fajriajjha@gmail.com>
2020-11-23 18:25:54 +0700

    feat: refresh token controller

---
+++ src/controllers/RefreshToken/controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express'
+import routes from 'routes/public'
+import asyncHandler from 'helpers/asyncHandler'
+import Authorization from 'middlewares/Authorization'
+import BuildResponse from 'modules/Response/BuildResponse'
+import RefreshToken from 'controllers/RefreshToken/service'
+
+routes.get(
+  '/refresh-token',
+  asyncHandler(async function getToken(req: Request, res: Response) {
+    const { refreshToken } = req.getQuery()
+    const data = await RefreshToken.getToken(refreshToken)
+    const buildResponse = BuildResponse.get({ data })
+
+    return res.status(200).json(buildResponse)
+  })
+)
+
+routes.post(
+  '/refresh-token',
+  Authorization,
+  asyncHandler(async function createData(req: Request, res: Response) {
+    const formData = req.getBody()
+
+    const data = await RefreshToken.create(formData)
+    const buildResponse = BuildResponse.created({ data })
+
+    return res.status(201).json(buildResponse)
+  })
+)
