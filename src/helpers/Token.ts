35c714bc5116413d170d0c8aa60055bb5309aaf4
masb0ymas <fajriajjha@gmail.com>
2020-11-24 13:29:18 +0700

    feat: verify refresh token

--- src/helpers/Token.ts
+++ src/helpers/Token.ts
@@ -8,7 +8,7 @@ import { isEmpty } from 'lodash'
 
 require('dotenv').config()
 
-const { JWT_SECRET }: any = process.env
+const { JWT_SECRET_ACCESS_TOKEN, JWT_SECRET_REFRESH_TOKEN }: any = process.env
 
 /**
  *
@@ -17,9 +17,14 @@ const { JWT_SECRET }: any = process.env
 function getToken(headers: any) {
   if (headers && headers.authorization) {
     const parted = headers.authorization.split(' ')
-    if (parted.length === 2) {
-      return parted[1]
+
+    // Check Bearer xxx || JWT xxx
+    if (parted[0] === 'Bearer' || parted[0] === 'JWT') {
+      if (parted.length === 2) {
+        return parted[1]
+      }
     }
+
     return null
   }
   return null
@@ -49,7 +54,7 @@ function verifyToken(token: string) {
       return { data: null, message: 'Unauthorized!' }
     }
 
-    const data = jwt.verify(token, JWT_SECRET)
+    const data = jwt.verify(token, JWT_SECRET_ACCESS_TOKEN)
     return { data, message: 'Token is verify' }
   } catch (err) {
     if (err instanceof TokenExpiredError) {
@@ -66,4 +71,31 @@ function verifyToken(token: string) {
   }
 }
 
-export { getToken, currentToken, verifyToken }
+/**
+ *
+ * @param token
+ */
+function verifyRefreshToken(token: string) {
+  try {
+    if (!token) {
+      return { data: null, message: 'Unauthorized!' }
+    }
+
+    const data = jwt.verify(token, JWT_SECRET_REFRESH_TOKEN)
+    return { data, message: 'Token is verify' }
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+
+    if (err instanceof JsonWebTokenError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+
+    if (err instanceof NotBeforeError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+  }
+}
+
+export { getToken, currentToken, verifyToken, verifyRefreshToken }
