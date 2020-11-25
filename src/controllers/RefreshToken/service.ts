d83cb2fcface85900185513f0867f87cd4306e20
masb0ymas <fajriajjha@gmail.com>
2020-11-25 09:50:21 +0700

    feat: service refresh token - get access token

--- src/controllers/RefreshToken/service.ts
+++ src/controllers/RefreshToken/service.ts
@@ -1,11 +1,24 @@
 import models from 'models'
+import jwt from 'jsonwebtoken'
 import ResponseError from 'modules/Response/ResponseError'
 import useValidation from 'helpers/useValidation'
-import { RefreshTokenAttributes } from 'models/refreshtoken'
+import {
+  RefreshTokenAttributes,
+  verifyRefreshTokenAttributes,
+} from 'models/refreshtoken'
 import schema from 'controllers/RefreshToken/schema'
 import UserService from 'controllers/User/service'
+import { verifyRefreshToken } from 'helpers/Token'
+import { isObject } from 'lodash'
 
 const { RefreshToken } = models
+
+const { JWT_SECRET_ACCESS_TOKEN }: string | any = process.env
+
+const JWT_ACCESS_TOKEN_EXPIRED = process.env.JWT_ACCESS_TOKEN_EXPIRED || '7d'
+const expiredJwt = JWT_ACCESS_TOKEN_EXPIRED.replace(/(d)/g, '') // condition 1d / 7d (day) not include 4m (minutes)
+
+const expiresIn = Number(expiredJwt) * 24 * 60 * 60
 
 class RefreshTokenService {
   /**
@@ -30,14 +43,55 @@ class RefreshTokenService {
    */
   public static async create(formData: RefreshTokenAttributes) {
     const value = useValidation(schema.create, formData)
-
     const user = await UserService.getOne(formData.UserId)
+
     if (user) {
       const data = await RefreshToken.create(value)
       return data
     }
 
     throw new ResponseError.BadRequest('Something went wrong')
+  }
+
+  public static async getAccessToken(email: string, refreshToken: string) {
+    if (!email || !refreshToken) {
+      throw new ResponseError.BadRequest('invalid token')
+    }
+
+    const getToken = await this.getToken(refreshToken)
+    const verifyToken = verifyRefreshToken(getToken.token)
+
+    if (isObject(verifyToken?.data)) {
+      // @ts-ignore
+      const decodeToken: verifyRefreshTokenAttributes = verifyToken?.data
+
+      if (email !== decodeToken?.email) {
+        throw new ResponseError.BadRequest('email is not valid')
+      }
+
+      const payloadToken = {
+        id: decodeToken?.id,
+        nama: decodeToken?.nama,
+        email: decodeToken?.email,
+        active: decodeToken?.active,
+      }
+
+      // Access Token
+      const accessToken = jwt.sign(
+        JSON.parse(JSON.stringify(payloadToken)),
+        JWT_SECRET_ACCESS_TOKEN,
+        {
+          expiresIn,
+        }
+      )
+
+      console.log({ getToken, verifyToken, decodeToken, accessToken })
+
+      return { accessToken, expiresIn }
+    }
+
+    // @ts-ignore
+    throw new ResponseError.Unauthorized(`${verifyToken?.message}`)
   }
 }
 
