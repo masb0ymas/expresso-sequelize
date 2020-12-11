db4d6b9b4afad81343e44e411ff0dbe867a81b85
masb0ymas <fajriajjha@gmail.com>
2020-12-11 13:27:48 +0700

    fix: read model instance

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import fs from 'fs'
 import path from 'path'
-import models from 'models/_instance'
+import models from '../models/_instance'
 
 const pathEnv = path.resolve('.env')
 
