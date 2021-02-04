82475e7a711e64c06a88672a9b2a39001bebb044
masb0ymas <fajriajjha@gmail.com>
2021-02-04 21:40:02 +0700

    fix: wording routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,9 @@ const router = express.Router()
 /* Home Page. */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
   const buildResponse = BuildResponse.get({
-    message: 'Express TS, Powered by Nusantech',
+    message: 'Express Sequelize TS, Support by Nusantech',
+    github:
+      'https://github.com/masb0ymas/boilerplate-express-typescript-sequelize',
   })
   return res.json(buildResponse)
 })
@@ -21,10 +23,5 @@ router.get('/v1', function (req: Request, res: Response, next: NextFunction) {
 
 /* Declare Route */
 router.use('/v1', publicRoute)
-
-/* Not Found Page. */
-// router.get('*', function (req: Request, res: Response, next: NextFunction) {
-//   throw new ResponseError.NotFound('endpoint not found')
-// })
 
 export default router
