39eb6f8a28a03778232574b00e62c83bba8ce40f
masb0ymas <fajriajjha@gmail.com>
2021-02-07 13:09:01 +0700

    fix: handle error multer

--- src/middlewares/ExpressErrorResponse.ts
+++ src/middlewares/ExpressErrorResponse.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import ResponseError from 'modules/Response/ResponseError'
 import { isObject } from 'lodash'
+import multer from 'multer'
 
 function generateErrorResponseError(e: Error, code: Number) {
   return isObject(e.message) ? e.message : { code, message: e.message }
@@ -12,6 +13,10 @@ async function ExpressErrorResponse(
   res: Response,
   next: NextFunction
 ) {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json(generateErrorResponseError(err, 400))
+  }
+
   if (err instanceof ResponseError.BaseResponse) {
     return res
       .status(err.statusCode)
