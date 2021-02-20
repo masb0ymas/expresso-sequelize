331f543f2354383b3dd972c2a1a61e33f3e5251b
masb0ymas <fajriajjha@gmail.com>
2021-02-20 21:28:18 +0700

    fix: typo fetcher config

--- src/config/Fetcher.ts
+++ src/config/Fetcher.ts
@@ -4,6 +4,10 @@ import ResponseError from 'modules/Response/ResponseError'
 
 const AXIOS_TIMEOUT = process.env.AXIOS_TIMEOUT || 5000
 
+/**
+ *
+ * @param baseURL
+ */
 function createAxios(baseURL: string): AxiosInstance {
   const instanceAxios = axios.create({
     baseURL,
@@ -73,13 +77,14 @@ class FetchApi {
   }
 
   /**
-   * axios instance with auth token
+   * axios instance default
    */
   get default(): AxiosInstance {
     if (!this.axiosDefault) {
       this.axiosDefault = createAxios(this.baseUri)
       return this.axiosDefault
     }
+
     return this.axiosDefault
   }
 }
