e88ff9cc9417e523ff4aac8875b9aa12e174a802
masb0ymas <fajriajjha@gmail.com>
2021-03-18 20:36:26 +0700

    fix: console role job

--- src/jobs/RoleJob.ts
+++ src/jobs/RoleJob.ts
@@ -1,6 +1,7 @@
 import RoleService from 'controllers/Role/service'
 import cron from 'node-cron'
 import ConstRoles from 'constants/ConstRoles'
+import { isEmpty } from 'lodash'
 
 class RoleJob {
   /**
@@ -10,7 +11,7 @@ class RoleJob {
     // Run this job every 30 minutes
     const task = cron.schedule('30 * * * *', async () => {
       const data = await RoleService.getOne(ConstRoles.ID_UMUM)
-      console.log('Running task check get role by id', { data })
+      console.log('Running task check get role by id', !isEmpty(data))
     })
 
     return task
