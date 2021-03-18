c9d40438a99fae24169f8d9aee710cf1c8c770b5
masb0ymas <fajriajjha@gmail.com>
2021-03-18 20:36:03 +0700

    feat(helper): add redis

---
+++ src/helpers/Redis.ts
@@ -0,0 +1,75 @@
+import Redis, { Redis as RedisClient } from 'ioredis'
+import ms from 'ms'
+
+require('dotenv').config()
+
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env
+
+const timeoutRedis = ms('1d') / 1000
+const expiryMode = 'PX' // PX = miliseconds || EX = seconds. full documentation https://redis.io/commands/set
+
+class RedisProvider {
+  private client: RedisClient
+
+  constructor() {
+    this.client = new Redis({
+      host: REDIS_HOST,
+      port: Number(REDIS_PORT),
+      password: REDIS_PASSWORD || undefined,
+    })
+  }
+
+  /**
+   *
+   * @param key
+   * @param data
+   */
+  public async set(key: string, data: any) {
+    await this.client.set(key, JSON.stringify(data), expiryMode, timeoutRedis)
+  }
+
+  /**
+   *
+   * @param key
+   * @example
+   * // get('get-role')
+   */
+  public async get<T>(key: string): Promise<T | null> {
+    const data = await this.client.get(key)
+
+    if (!data) {
+      return null
+    }
+
+    const parseData = JSON.parse(data) as T
+    return parseData
+  }
+
+  /**
+   *
+   * @param key get all by key return array
+   * @example
+   * // keys.('get-role:*')
+   */
+  public async delete(key: string) {
+    await this.client.del(key)
+  }
+
+  /**
+   *
+   * @param key
+   */
+  public async deleteByPrefix(prefix: string) {
+    const keys = await this.client.keys(`${prefix}:*`)
+
+    const pipeline = this.client.pipeline()
+
+    keys.forEach((key) => {
+      pipeline.del(key)
+    })
+
+    await pipeline.exec()
+  }
+}
+
+export default RedisProvider
