2c8f22309ba5d19f92ef5054cb77603c24dc987d
masb0ymas <fajriajjha@gmail.com>
2021-03-18 19:31:22 +0700

    feat(middleware): add express rate limit

---
+++ src/middlewares/ExpressRateLimit.ts
@@ -0,0 +1,26 @@
+import { redisClient } from 'config/Rediss'
+import { NextFunction, Request, Response } from 'express'
+import { RateLimiterRedis } from 'rate-limiter-flexible'
+
+// Rate Limit Request
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'middleware',
+  points: 10, // 10 requests
+  duration: 1, // per 1 second by IP
+})
+
+async function ExpressRateLimit(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    await rateLimiter.consume(req.ip)
+    return next()
+  } catch (err) {
+    return res.status(429).json({ code: 429, message: 'Too Many Requests' })
+  }
+}
+
+export default ExpressRateLimit
