9626cdd34089c23e5686393cbebb336ee8ae7d6a
masb0ymas <fajriajjha@gmail.com>
2021-04-19 15:01:27 +0700

    fix: seeder roles

--- src/seeders/20200724140104-Seeder-Role.js
+++ src/seeders/20200724140104-Seeder-Role.js
@@ -19,11 +19,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Roles', null, {})
   },
 }
