ea25b4127b8793398507f17cd442bdc0a93f38b5
masb0ymas <fajriajjha@gmail.com>
2021-06-07 16:44:03 +0700

    feat: add env rate limit

--- src/middlewares/ExpressRateLimit.ts
+++ src/middlewares/ExpressRateLimit.ts
@@ -2,11 +2,15 @@ import redisClient from 'config/redisClient'
 import { NextFunction, Request, Response } from 'express'
 import { RateLimiterRedis } from 'rate-limiter-flexible'
 
+require('dotenv').config()
+
+const RATE_LIMIT = Number(process.env.RATE_LIMIT) || 50
+
 // Rate Limit Request
 const rateLimiter = new RateLimiterRedis({
   storeClient: redisClient,
   keyPrefix: 'middleware',
-  points: 10, // 10 requests
+  points: RATE_LIMIT, // 10 requests
   duration: 1, // per 1 second by IP
 })
 
