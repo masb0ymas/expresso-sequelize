033d104f448c6ea9ba492cefe6ce28f276ef45f5
masb0ymas <fajriajjha@gmail.com>
2021-06-13 15:22:30 +0700

    fix: pm2 cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'api_express_ts',
-      script: 'npm run serve:production',
-
-      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      exec_mode: 'fork',
-      instances: 1,
-      autorestart: false,
-      watch: false,
-      max_memory_restart: '1G',
+      name: 'api_expresso',
+      script: './dist/bin/server.js',
+      instances: 'max', // max instance = 0 | max
+      exec_mode: 'cluster',
+      watch: '.', // default: watch = true
+      env: {
+        NODE_ENV: 'staging',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
   ],
-
-  // deploy : {
-  //  production : {
-  //    user : 'SSH_USERNAME',
-  //    host : 'SSH_HOSTMACHINE',
-  //    ref  : 'origin/master',
-  //    repo : 'GIT_REPOSITORY',
-  //    path : 'DESTINATION_PATH',
-  //    'pre-deploy-local': '',
-  //    'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-  //    'pre-setup': ''
-  //  }
-  // }
 }
