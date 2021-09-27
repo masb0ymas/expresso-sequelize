c464d067aa4c62d2a2edbd884c9b9a862910f2c3
masb0ymas <fajriajjha@gmail.com>
2021-09-27 18:27:30 +0700

    feat: add endpoint health

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -28,6 +28,16 @@ route.get('/', function (req: Request, res: Response) {
   return res.json(httpResponse)
 })
 
+route.get('/health', function (req: Request, res: Response) {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date(),
+  }
+
+  res.status(200).json({ status: data })
+})
+
 /* Forbidden Page. */
 route.get('/v1', function (req: Request, res: Response) {
   throw new ResponseError.Forbidden(
