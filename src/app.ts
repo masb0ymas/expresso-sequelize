fabcca67064bebc490d231eea8c4e322d8b34bb3
masb0ymas <fajriajjha@gmail.com>
2021-10-20 19:41:56 +0700

    fix: update eslint & typescript eslint

--- src/app.ts
+++ src/app.ts
@@ -160,9 +160,11 @@ class App {
         case 'EACCES':
           console.error(`${bind} requires elevated privileges`)
           process.exit(1)
+          break
         case 'EADDRINUSE':
           console.error(`${bind} is already in use`)
           process.exit(1)
+          break
         default:
           throw error
       }
