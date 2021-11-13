44c72abeee6e1c5da69c769e1ce9a6057cbab6d2
N. Fajri <fajriajjha@gmail.com>
2021-11-13 20:27:18 +0700

    fix: add get uniq code

--- src/@expresso/helpers/Formatter.ts
+++ src/@expresso/helpers/Formatter.ts
@@ -75,4 +75,26 @@ function validateUUID(value: string): string {
   return value
 }
 
-export { arrayFormatter, validateEmpty, validateBoolean, validateUUID }
+/**
+ *
+ * @param length
+ * @returns
+ */
+function getUniqueCodev2(length = 32): string {
+  let result = ''
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
+
+export {
+  arrayFormatter,
+  validateEmpty,
+  validateBoolean,
+  validateUUID,
+  getUniqueCodev2,
+}
