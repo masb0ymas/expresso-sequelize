4c80b21e0642553a6f3e3ac8e702d031636991e7
N. Fajri <fajriajjha@gmail.com>
2021-11-28 17:21:26 +0700

    fix: annotation compiler date helper

--- src/@expresso/helpers/Date.ts
+++ src/@expresso/helpers/Date.ts
@@ -3,10 +3,20 @@ import { id } from 'date-fns/locale'
 
 const TZ_ID = { locale: id } // Timezone Indonesia
 
+/**
+ *
+ * @param date
+ * @returns {string}
+ */
 const formatDate = (date: Date | number | string): string => {
   return format(new Date(date), 'dd-MM-yyyy', TZ_ID)
 }
 
+/**
+ *
+ * @param date
+ * @returns {string}
+ */
 const formatDateTime = (date: Date | number | string): string => {
   return format(new Date(date), 'dd-MM-yyyy HH:mm:ss', TZ_ID)
 }
