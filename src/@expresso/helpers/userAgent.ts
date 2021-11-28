87f9584e89c169ccf8a6a765df108b62041f7a67
N. Fajri <fajriajjha@gmail.com>
2021-11-28 17:24:56 +0700

    fix: improve user agent helper

--- src/@expresso/helpers/userAgent.ts
+++ src/@expresso/helpers/userAgent.ts
@@ -1,28 +1,48 @@
 import { Request } from 'express'
 
+const invalidPlatform = ['', null, 'null', undefined, 'undefined', 'unknown']
+
 class userAgentHelper {
   /**
    *
    * @param req
-   * @returns
+   * @returns {string}
    */
-  public static currentDevice(req: Request): string | null {
+  public static currentDevice(req: Request): string {
     const { useragent } = req
+
+    const checkMobileAccess =
+      useragent?.isMobile ??
+      useragent?.isBlackberry ??
+      useragent?.isAndroid ??
+      useragent?.isiPhone ??
+      useragent?.isWindowsPhone
+
+    const checkTabletAccess =
+      useragent?.isTablet ?? useragent?.isAndroidTablet ?? useragent?.isiPad
+
+    const checkDesktopAccess =
+      useragent?.isDesktop ??
+      useragent?.isChromeOS ??
+      useragent?.isLinux ??
+      useragent?.isLinux64 ??
+      useragent?.isWindows ??
+      useragent?.isMac
 
     let device = null
 
-    if (
-      useragent?.isTablet ??
-      useragent?.isAndroidTablet ??
-      useragent?.isiPad
-    ) {
+    if (checkTabletAccess) {
       device = 'Tablet'
-    } else if (useragent?.isMobile ?? useragent?.isBlackberry) {
+    } else if (checkMobileAccess) {
       device = 'Mobile'
     } else if (useragent?.isSmartTV) {
       device = 'Smart TV'
-    } else if (useragent?.isDesktop) {
+    } else if (checkDesktopAccess) {
       device = 'Desktop'
+    } else if (useragent?.isSamsung) {
+      device = 'Samsung'
+    } else {
+      device = 'unkown'
     }
 
     return device
@@ -31,14 +51,14 @@ class userAgentHelper {
   /**
    *
    * @param req
-   * @returns
+   * @returns {string}
    */
   public static currentPlatform(req: Request): string {
     const { useragent } = req
 
     let currentOS = null
 
-    if (useragent?.os === 'unkown') {
+    if (invalidPlatform.includes(useragent?.os)) {
       currentOS = this.currentDevice(req)
     } else {
       currentOS = useragent?.os
