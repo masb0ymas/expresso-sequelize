2a8945b6c00339c300d61283ea252e9d25d1eb34
N. Fajri <fajriajjha@gmail.com>
2022-02-20 13:36:26 +0700

    fix: translate permission access

--- src/middlewares/NotPermittedAccess.ts
+++ src/middlewares/NotPermittedAccess.ts
@@ -1,10 +1,17 @@
+import { i18nConfig } from '@config/i18nextConfig'
 import User, { UserLoginAttributes } from '@database/models/user'
 import { logErrServer } from '@expresso/helpers/Formatter'
 import HttpResponse from '@expresso/modules/Response/HttpResponse'
 import { NextFunction, Request, Response } from 'express'
+import { TOptions } from 'i18next'
 
 function NotPermittedAccess(roles: string[]) {
   return async (req: Request, res: Response, next: NextFunction) => {
+    const { lang } = req.getQuery()
+    const defaultLang = lang ?? 'en'
+
+    const i18nOpt: string | TOptions = { lng: defaultLang }
+
     const userLogin = req.getState('userLogin') as UserLoginAttributes
     const getUser = await User.findByPk(userLogin.uid)
 
@@ -15,9 +22,10 @@ function NotPermittedAccess(roles: string[]) {
       // log error
       console.log(logErrServer(errType, message))
 
+      const errMessage = i18nConfig.t('errors.permissionAccess', i18nOpt)
       const httpResponse = HttpResponse.get({
         code: 403,
-        message: `${errType} ${message}`,
+        message: errMessage,
       })
 
       return res.status(403).json(httpResponse)
