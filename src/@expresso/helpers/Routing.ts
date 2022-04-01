e8cd28998fd8fd653b50d7f2c6ad67e7f853bb11
N. Fajri <fajriajjha@gmail.com>
2022-04-01 16:27:13 +0700

    fix: update helper routing

--- src/@expresso/helpers/Routing.ts
+++ src/@expresso/helpers/Routing.ts
@@ -8,18 +8,41 @@ import fs from 'fs'
  */
 export const getRoutes = (basePath: string | Buffer): void => {
   const msgType = 'Route'
-  // loop directory
+  // loop main controller directory
   return fs.readdirSync(basePath).forEach((file) => {
-    // read controller
+    // read controller on main controller directory
     const getController = `${basePath}/${file}/controller`
 
-    // check dir controller exists
+    // check sub dir controller exists
     if (!fs.existsSync(`${getController}.js`)) {
       const subDir = `${basePath}/${file}`
-      // loop sub directory
+
+      // loop sub directory controller
       fs.readdirSync(subDir).forEach((subFile) => {
         // read controller
-        const getSubController = `${basePath}/${file}/${subFile}/controller`
+        const getSubController = `${subDir}/${subFile}/controller`
+
+        // check sub-sub dir controller exists
+        if (!fs.existsSync(`${getSubController}.js`)) {
+          const sub2Dir = `${basePath}/${file}/${subFile}`
+
+          // loop sub directory
+          fs.readdirSync(sub2Dir).forEach((sub2File) => {
+            // read controller
+            const getSub2Controller = `${sub2Dir}/${sub2File}/controller`
+
+            // check sub dir controller exists
+            if (fs.existsSync(`${getSub2Controller}.js`)) {
+              const routeSub2Dir = chalk.cyan(`${file}/${subFile}/${sub2File}`)
+              const message = `Controller ${routeSub2Dir} Registered`
+
+              console.log(logServer(msgType, message))
+
+              // require controller
+              require(getSub2Controller)
+            }
+          })
+        }
 
         // check sub dir controller exists
         if (fs.existsSync(`${getSubController}.js`)) {
