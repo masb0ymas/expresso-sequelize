ae1243456a2693754af18381d38538daab0c9f3c
masb0ymas <n.fajri@outlook.com>
2023-04-04 10:59:03 +0700

    perf: improve scripts

--- src/scripts/generate.ts
+++ src/scripts/generate.ts
@@ -1,28 +1,8 @@
 import chalk from 'chalk'
+import { printLog, randomString } from 'expresso-core'
 import fs from 'fs'
 import path from 'path'
 
-/**
- *
- * @param length
- * @returns
- */
-const getUniqueCodev2 = (length = 32): string => {
-  let result = ''
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  const charactersLength = characters.length
-  for (let i = 0; i < length; i += 1) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength))
-  }
-  return result
-}
-
-/**
- *
- * @param value
- * @param regExp
- */
 function generateEnv(value: string, regExp: RegExp): void {
   const pathRes = path.resolve('.env')
 
@@ -36,39 +16,35 @@ function generateEnv(value: string, regExp: RegExp): void {
   }
 
   const contentEnv = fs.readFileSync(pathRes, { encoding: 'utf-8' })
-  const uniqueCode = getUniqueCodev2()
+
+  const uniqueCode = randomString.generate()
   const valueEnv = `${value}=${uniqueCode}`
 
   if (contentEnv.includes(`${value}=`)) {
     // change value
     const replaceContent = contentEnv.replace(regExp, valueEnv)
     fs.writeFileSync(`${pathRes}`, replaceContent)
-    console.log(`Refresh ${chalk.cyan(valueEnv)} Success`)
+
+    const logMessage = printLog(`Refresh ${value}`, `= ${uniqueCode}`)
+    console.log(logMessage)
   } else {
     // Generate value
     const extraContent = `${valueEnv}\n\n${contentEnv}`
     fs.writeFileSync(`${pathRes}`, extraContent)
-    console.log(`Generate ${chalk.cyan(valueEnv)} Success`)
+
+    const logMessage = printLog(`Generate ${value}`, `= ${uniqueCode}`)
+    console.log(logMessage)
   }
 }
 
-function appKey(): void {
-  generateEnv('APP_KEY', /APP_KEY=(.*)?/)
-}
+// generate app key
+generateEnv('APP_KEY', /APP_KEY=(.*)?/)
 
-function secretOTP(): void {
-  generateEnv('SECRET_OTP', /SECRET_OTP=(.*)?/)
-}
+// generate secret otp
+generateEnv('SECRET_OTP', /SECRET_OTP=(.*)?/)
 
-function secretAccessToken(): void {
-  generateEnv('JWT_SECRET_ACCESS_TOKEN', /JWT_SECRET_ACCESS_TOKEN=(.*)?/)
-}
+// generate jwt secret access token
+generateEnv('JWT_SECRET_ACCESS_TOKEN', /JWT_SECRET_ACCESS_TOKEN=(.*)?/)
 
-function secretRefreshToken(): void {
-  generateEnv('JWT_SECRET_REFRESH_TOKEN', /JWT_SECRET_REFRESH_TOKEN=(.*)?/)
-}
-
-appKey()
-secretOTP()
-secretAccessToken()
-secretRefreshToken()
+// generate jwt secret refresh token
+generateEnv('JWT_SECRET_REFRESH_TOKEN', /JWT_SECRET_REFRESH_TOKEN=(.*)?/)
