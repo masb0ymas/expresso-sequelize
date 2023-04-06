ddbbd49178963b0047383914fc55618a254a2c8c
masb0ymas <n.fajri@outlook.com>
2023-04-06 10:56:50 +0700

    fix: remove unuse file

--- src/core/interface/QueryFiltered.ts
+++ src/core/interface/QueryFiltered.ts
@@ -1,11 +1,4 @@
-import type SqlizeQuery from '@core/hooks/Query/SqlizeQuery'
-import {
-  type IncludeOptions,
-  type Includeable,
-  type ModelStatic,
-  type Order,
-  type WhereOptions,
-} from 'sequelize'
+import { type Includeable, type Order, type WhereOptions } from 'sequelize'
 
 export interface FilteredQueryEntity {
   id: string
@@ -15,43 +8,6 @@ export interface FilteredQueryEntity {
 export interface SortedQueryEntity {
   sort: string
   order: 'ASC' | 'DESC'
-}
-
-export interface FilterIncludeHandledOnlyEntity {
-  include: any
-  filteredInclude?: any
-}
-
-export interface IncludeFilteredQueryEntity {
-  filteredValue: any
-  model: any
-  prefixName: any
-  options?: IncludeOptions
-}
-
-export interface ReqGenerate {
-  filtered?: FilteredQueryEntity[]
-  sorted?: SortedQueryEntity[]
-  page?: number
-  pageSize?: number
-  [key: string]: any
-}
-
-export interface OnBeforeBuildQuery {
-  paginationQuery: SqlizeQuery
-  filteredQuery: SqlizeQuery
-  sortedQuery: SqlizeQuery
-}
-
-export interface QueryOptions {
-  onBeforeBuild: (query: OnBeforeBuildQuery) => void
-}
-
-export interface useQueryEntity {
-  entity: ModelStatic<any>
-  reqQuery: ReqGenerate
-  includeRule?: Includeable | Includeable[]
-  options?: QueryOptions
 }
 
 export interface DtoQueryEntity {
