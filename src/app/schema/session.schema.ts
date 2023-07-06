9abc052976d4ff2380846df697b72653386dce6f
masb0ymas <n.fajri@outlook.com>
2023-07-06 16:23:04 +0700

    fix: change yup to zod

--- src/app/schema/session.schema.ts
+++ src/app/schema/session.schema.ts
@@ -1,16 +1,45 @@
-import * as yup from 'yup'
+import { z } from 'zod'
 
-const create = yup
-  .object({
-    user_id: yup.string().required('user id is required'),
-    token: yup.string().required('token is required'),
-    ip_address: yup.string().required('ip address is required'),
-    device: yup.string().required('device is required'),
-    platform: yup.string().required('platform is required'),
-    latitude: yup.string().nullable(),
-    longitude: yup.string().nullable(),
-  })
-  .required()
+const create = z.object({
+  user_id: z
+    .string({
+      required_error: 'user_id is required',
+      invalid_type_error: 'user_id must be a string',
+    })
+    .uuid({ message: 'user_id invalid uuid format' })
+    .min(2, `user_id can't be empty`),
+
+  token: z
+    .string({
+      required_error: 'token is required',
+      invalid_type_error: 'token must be a string',
+    })
+    .min(2, `token can't be empty`),
+
+  ip_address: z
+    .string({
+      required_error: 'ip_address is required',
+      invalid_type_error: 'ip_address must be a string',
+    })
+    .min(2, `ip_address can't be empty`),
+
+  device: z
+    .string({
+      required_error: 'device is required',
+      invalid_type_error: 'device must be a string',
+    })
+    .min(2, `device can't be empty`),
+
+  platform: z
+    .string({
+      required_error: 'platform is required',
+      invalid_type_error: 'platform must be a string',
+    })
+    .min(2, `platform can't be empty`),
+
+  latitude: z.string().nullable(),
+  longitude: z.string().nullable(),
+})
 
 const sessionSchema = { create }
 
--- src/app/schema/upload.schema.ts
+++ src/app/schema/upload.schema.ts
@@ -1,15 +1,46 @@
-import * as yup from 'yup'
+import { z } from 'zod'
 
-const create = yup
-  .object({
-    key_file: yup.string().required('key file is required'),
-    filename: yup.string().required('filename is required'),
-    mimetype: yup.string().required('mimetype is required'),
-    size: yup.number().required('size is required'),
-    signed_url: yup.string().required('signed url is required'),
-    expiry_date_url: yup.date().required('expiry date url is required'),
-  })
-  .required()
+const create = z.object({
+  key_file: z
+    .string({
+      required_error: 'key_file is required',
+      invalid_type_error: 'key_file must be a string',
+    })
+    .min(2, `key_file can't be empty`),
+
+  filename: z
+    .string({
+      required_error: 'filename is required',
+      invalid_type_error: 'filename must be a string',
+    })
+    .min(2, `filename can't be empty`),
+
+  mimetype: z
+    .string({
+      required_error: 'mimetype is required',
+      invalid_type_error: 'mimetype must be a string',
+    })
+    .min(2, `mimetype can't be empty`),
+
+  size: z
+    .number({
+      required_error: 'size is required',
+      invalid_type_error: 'size must be a number',
+    })
+    .min(2, `size can't be empty`),
+
+  signed_url: z
+    .string({
+      required_error: 'signed_url is required',
+      invalid_type_error: 'signed_url must be a string',
+    })
+    .min(2, `signed_url can't be empty`),
+
+  expiry_date_url: z.date({
+    required_error: 'expiry_date_url is required',
+    invalid_type_error: 'expiry_date_url must be a date',
+  }),
+})
 
 const uploadSchema = { create }
 
