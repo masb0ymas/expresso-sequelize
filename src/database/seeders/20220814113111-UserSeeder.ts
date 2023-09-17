42a64b50a9bede085af89715873d36c6ebd91654
masb0ymas <n.fajri@outlook.com>
2023-09-17 19:10:00 +0700

    fix: improve seeder

--- src/database/seeders/20220814113111-UserSeeder.ts
+++ src/database/seeders/20220814113111-UserSeeder.ts
@@ -1,14 +1,11 @@
-'use strict'
-
-import { hashSync } from 'bcrypt'
 import { green } from 'colorette'
 import _ from 'lodash'
 import { DataTypes, QueryInterface } from 'sequelize'
 import { v4 as uuidv4 } from 'uuid'
+import { hashing } from '~/config/hashing'
 import { logger } from '~/config/pino'
 import { default as ConstRole } from '~/core/constants/ConstRole'
 
-const salt = 10
 const defaultPassword = 'Padang123'
 
 logger.info(`Seed - your default password: ${green(defaultPassword)}`)
@@ -31,27 +28,29 @@ const data = [
   },
 ]
 
-const formData: any[] = []
-
-if (!_.isEmpty(data)) {
-  for (let i = 0; i < data.length; i += 1) {
-    const item = data[i]
-
-    formData.push({
-      ...item,
-      id: uuidv4(),
-      is_active: true,
-      password: hashSync(defaultPassword, salt),
-      created_at: new Date(),
-      updated_at: new Date(),
-    })
-  }
-}
-
 export async function up(
   queryInterface: QueryInterface,
   Sequelize: typeof DataTypes
 ) {
+  const password = await hashing.hash(defaultPassword)
+
+  const formData: any[] = []
+
+  if (!_.isEmpty(data)) {
+    for (let i = 0; i < data.length; i += 1) {
+      const item = data[i]
+
+      formData.push({
+        ...item,
+        id: uuidv4(),
+        is_active: true,
+        password,
+        created_at: new Date(),
+        updated_at: new Date(),
+      })
+    }
+  }
+
   await queryInterface.bulkInsert('user', formData)
 }
 
