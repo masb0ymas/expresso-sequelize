13ee2989fc14f5a5870bc7c5fb3f6dae1eb769a8
masb0ymas <n.fajri@outlook.com>
2024-01-24 11:11:56 +0700

    fix: minor bug update package

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,26 +1,18 @@
 import { SMTP } from 'expresso-provider'
-import { type MailDriverType } from 'expresso-provider/lib/smtp'
 import { env } from './env'
 
 /**
  * Initialize Mail Service Config
  */
-export const mailService = new SMTP({
-  // Your App Name
-  appName: env.APP_NAME,
-
-  // Mail Driver support 'smtp' | 'gmail'
-  driver: env.MAIL_DRIVER as MailDriverType,
-
-  // Mail Host
-  host: env.MAIL_HOST,
-
-  // Mail Port
-  port: env.MAIL_PORT,
-
-  // Mail Username
-  username: env.MAIL_USERNAME,
-
-  // Mail Password
-  password: env.MAIL_PASSWORD,
-})
+export const mailService = new SMTP(
+  { from: `${env.APP_NAME} <${env.MAIL_USERNAME}>` },
+  {
+    service: env.MAIL_DRIVER,
+    host: env.MAIL_HOST,
+    port: env.MAIL_PORT,
+    auth: {
+      user: env.MAIL_USERNAME,
+      pass: env.MAIL_PASSWORD,
+    },
+  }
+)
