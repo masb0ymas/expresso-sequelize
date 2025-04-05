f78206086c93de9620a0d2235398db1880e228cc
masb0ymas <n.fajri@outlook.com>
2025-04-05 20:48:46 +0700

    fix: update variable secret

--- script/secret.sh
+++ script/secret.sh
@@ -16,7 +16,7 @@ echo " - Detecting OS type: $OS_TYPE"
 echo " - Detecting OS version: $OS_VERSION"
 
 # Generate secure values
-TYPEORM_PASSWORD=$(openssl rand -hex 16)
+SEQUELIZE_PASSWORD=$(openssl rand -hex 16)
 APP_DEFAULT_PASS=$(openssl rand -hex 8)
 APP_SECRET=$(openssl rand -base64 32)
 JWT_SECRET=$(openssl rand -base64 32)
@@ -41,7 +41,7 @@ if [ -f "$BASE_ENV_FILE" ]; then
   echo " - Copy $BASE_ENV_FILE to $ENV_FILE"
 
   # Update values in .env file
-  $SED_CMD "s|^TYPEORM_PASSWORD=.*|TYPEORM_PASSWORD='$TYPEORM_PASSWORD'|" "$ENV_FILE"
+  $SED_CMD "s|^SEQUELIZE_PASSWORD=.*|SEQUELIZE_PASSWORD='$SEQUELIZE_PASSWORD'|" "$ENV_FILE"
   $SED_CMD "s|^APP_DEFAULT_PASS=.*|APP_DEFAULT_PASS='$APP_DEFAULT_PASS'|" "$ENV_FILE"
   $SED_CMD "s|^APP_SECRET=.*|APP_SECRET='$APP_SECRET'|" "$ENV_FILE"
   $SED_CMD "s|^JWT_SECRET=.*|JWT_SECRET='$JWT_SECRET'|" "$ENV_FILE"
